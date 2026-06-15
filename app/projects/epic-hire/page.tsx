7d0489a2dc7eb671d0dc1c0d57093ce856ebc622
Ava Vellozzi <108422751+AvaVellozzi@users.noreply.github.com>
2026-06-15 19:31:24 -0400

    fixed build fail

---
+++ app/projects/epic-hire/page.tsx
@@ -0,0 +1,8 @@
+export default function EpicHirePage() {
+  return (
+    <main>
+      <h1>Epic Hire</h1>
+      <p>This is my epic hire project page.</p>
+    </main>
+  );
+}
---
+++ app/projects/labspec/page.tsx
@@ -0,0 +1,8 @@
+export default function LabspecPage() {
+  return (
+    <main>
+      <h1>Labspec</h1>
+      <p>This is my Labspec project page.</p>
+    </main>
+  );
+}
